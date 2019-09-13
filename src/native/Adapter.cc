11052094baba9e44a88a7d09dba66568516d7a58
Jerônimo Nunes Rocha <jeronimonunes@live.com>
2019-09-13 17:03:39 -0300

    Moving adaptive code to cpp side to not have to manage memory from the JS side

--- src/native/Adapter.cc
+++ src/native/Adapter.cc
@@ -3,35 +3,58 @@
 
 using namespace emscripten;
 
+Vector jsFractionArrayToVector(val b)
+{
+    Vector v(b["length"].as<unsigned int>());
+    for (unsigned int i = 0; i < v.size(); i++)
+    {
+        v[i] = b[i].as<Fraction>();
+    }
+    return v;
+}
+
+Matrix jsFractionArrayArrayToMatrix(val b)
+{
+    Matrix m;
+    unsigned int size = b["length"].as<unsigned int>();
+    for (unsigned int i = 0; i < size; i++)
+    {
+        m.push_back(jsFractionArrayToVector(b[i]));
+    }
+    return m;
+}
+
+val simplex(val a, val b, val c)
+{
+    Matrix A = jsFractionArrayArrayToMatrix(a);
+    Vector B = jsFractionArrayToVector(b);
+    Vector C = jsFractionArrayToVector(c);
+    Tabloid tabloid(A, B, C);
+    Result result = runSimplex(tabloid);
+    val res = val::object();
+    res.set("solution", val::array(result.solution));
+    res.set("certificate", val::array(result.certificate));
+    res.set("value", result.value);
+    switch (result.type)
+    {
+    case ResultType::ILIMITED:
+        res.set("type", "ILIMITED");
+        break;
+    case ResultType::LIMITED:
+        res.set("type", "LIMITED");
+        break;
+    case ResultType::UNFEASIBLE:
+        res.set("type", "UNFEASIBLE");
+        break;
+    }
+    return res;
+}
+
 EMSCRIPTEN_BINDINGS(simplex)
 {
+    emscripten::function("simplex", &simplex);
+
     value_object<Fraction>("Fraction")
         .field("numerator", &Fraction::getNumerator, &Fraction::setNumerator)
         .field("denominator", &Fraction::getDenominator, &Fraction::setDenominator);
-
-    register_vector<Fraction>("vector<Fraction>");
-    class_<Vector, base<std::vector<Fraction>>>("Vector")
-        .constructor<>()
-        .constructor<int>()
-        .constructor<Fraction, int>();
-
-    register_vector<Vector>("vector<Vector>");
-    class_<Matrix, base<std::vector<Vector>>>("Matrix")
-        .constructor<>();
-
-    class_<Tabloid>("Tabloid")
-        .constructor<Matrix, Vector, Vector>();
-
-    class_<Result>("Result")
-        .property("certificate", &Result::certificate)
-        .property("solution", &Result::solution)
-        .property("type", &Result::type)
-        .property("value", &Result::value);
-    
-    enum_<ResultType>("ResultType")
-        .value("ILIMITED", ResultType::ILIMITED)
-        .value("LIMITED", ResultType::LIMITED)
-        .value("UNFEASIBLE", ResultType::UNFEASIBLE);
-
-    emscripten::function("runSimplex", &runSimplex);
 }
--- src/native/simplex.d.ts
+++ src/native/simplex.d.ts
@@ -1,64 +1,11 @@
-declare const Type: FunctionConstructor;
-
-declare interface Type<T> extends Function {
-    new(...args: any[]): T;
-}
-
-declare class NativeClass {
-    delete: () => void;
-}
-
-declare class StdVector<T> extends NativeClass {
-    constructor();
-    push_back: (v: T) => void;
-    size: () => number;
-    get: (i: number) => T;
-    set: (idx: number, v: T) => void;
-    resize: (size: number, v: T) => void;
-}
-
 export interface NativeFraction {
     numerator: string;
     denominator: string;
 }
 
-export declare class Vector extends StdVector<NativeFraction> {
-
-}
-
-export declare class Matrix extends StdVector<Vector> {
-
-}
-
-export declare class Tabloid extends NativeClass {
-
-}
-
-export class EnumConstant {
-    
-}
-
-declare class ResultType {
-    LIMITED: EnumConstant;
-    UNFEASIBLE: EnumConstant;
-    ILIMITED: EnumConstant;
-}
-
-export declare class Result extends NativeClass {
-    type: ResultType;
-    certificate: Vector;
-    value: NativeFraction;
-    solution: Vector;
-}
-
 export declare class SimplexNativeModule {
 
-    Vector: Type<Vector>;
-    Matrix: Type<Matrix>;
-    Tabloid: Type<Tabloid>;
-    ResultType: ResultType;
-
-    runSimplex: (tabloid: Tabloid) => Result;
+    simplex: (...args: any[]) => any;
 
 }
 
